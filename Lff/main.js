0ba09d81f0700ffe7108543f4e41483f0da97538
Madina Bekisheva <madinabekisheva@MacBook-Pro-Madina-2.local>
2022-10-25 11:19:55 +0600

    Initial Commit

---
+++ Lff/main.js
@@ -0,0 +1,58 @@
+// Declaration of a function for 3rd exercise
+function AreaOfaCircle (a) {
+console.log(a*a*3.14); 
+}
+
+//Invocation 
+AreaOfaCircle (3);
+
+AreaOfaCircle (4);
+
+//Declaration of a function for 15th exercise
+function DayOfTheWeek (a) {
+  if(a==1) {
+    console.log ("Monday")
+  }
+  if(a==2) {
+    console.log ("Tuesday")
+  }
+    if(a==3) {
+    console.log ("Wednesday")
+  }
+  if(a==4) {
+    console.log ("Thursday")
+  }
+  if(a==5) {
+    console.log ("Friday")
+  }
+  if(a==6) {
+    console.log ("Saturday")
+  }
+  if(a==7) {
+    console.log ("Sunday")
+  }
+}
+
+//Invocation
+DayOfTheWeek (7);
+
+DayOfTheWeek (5);
+
+
+
+//Declaration of a function for 6th exercise
+function SumOfSquares (a,b) {
+console.log(a*a+b*b); 
+}
+
+//Invocation
+SumOfSquares (1,2);
+
+SumOfSquares (3,4);
+
+
+
+
+
+
+
